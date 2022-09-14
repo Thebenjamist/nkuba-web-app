c27e2d1566029d311f8a5575bcdf266a0eb5276e
Benjamin Ho <benho061995@gmail.com>
2022-09-14 13:57:11 +0200

    Created home page

--- pages/index.js
+++ pages/index.js
@@ -1,105 +1,74 @@
 import * as React from "react";
-import {
-  Box,
-  Button,
-  Container,
-  Grid,
-  Paper,
-  TextField,
-  Typography,
-} from "@mui/material";
-
-import AppBar from "../components/organisms/AppBarTop";
-import Head from "next/head";
-import Image from "next/image";
+import { Grid, Paper, Typography } from "@mui/material";
 import OrderForm from "../components/organisms/OrderForm";
+import Layout from "../components/organisms/Layout";
 
 const Home = () => {
   return (
     <>
-      <Head>
-        <title>Nkuba Logistics</title>
-        <link rel="shortcut icon" href="images/icons/bolt.png" />
-      </Head>
-
-      <Box
-        sx={{
-          backgroundColor: "primary.main",
-          width: "100%",
-        }}
-      >
-        <AppBar />
-
-        <Container
+      <Layout>
+        <Grid
+          container
+          // style={{ height: "100%" }}
+          // direction={{ xs: "column-reverse", md: "row" }}
           sx={{
-            padding: "20px",
-            minHeight: "calc(100vh - 100px)",
+            height: "100%",
+            display: "flex",
             justifyContent: "center",
             alignItems: "center",
-            alignContent: "center",
-            display: "flex",
+            flexDirection: "column",
+            width: "100%",
           }}
         >
-          <Grid
-            container
-            style={{ height: "100%" }}
+          {/* <Grid
+            item
+            xs={12}
+            md={6}
             sx={{
-              height: "100%",
-              display: "flex",
-              justifyContent: "center",
               alignItems: "center",
+              justifyContent: "center",
+              alignContent: "center",
+              display: "flex",
+              padding: "16px",
             }}
           >
-            <Grid
-              item
-              xs={12}
-              md={6}
+            <Paper
               sx={{
-                alignItems: "center",
-                justifyContent: "center",
-                alignContent: "center",
-                display: "flex",
-                padding: "16px",
-              }}
-            >
-              <Paper
-                sx={{
-                  height: 300,
-                  width: 300,
-                  padding: "20px",
-                  margin: 0,
-                  textAlign: "center",
-                  backgroundColor: "secondary.main",
-                }}
-              >
-                <Typography variant="h4" color="background.paper">
-                  Specials
-                </Typography>
-                <Typography variant="h6" color="background.paper">
-                  K40 for all deliveries in Lusaka
-                </Typography>
-              </Paper>
-            </Grid>
-
-            <Grid
-              item
-              xs={12}
-              md={6}
-              sx={{
-                alignItems: "center",
-                justifyContent: "center",
-                alignContent: "center",
-                display: "flex",
+                height: 300,
+                maxWidth: 300,
                 padding: "20px",
-                flexDirection: "column",
+                margin: 0,
+                textAlign: "center",
+                backgroundColor: "secondary.main",
               }}
             >
-              <Typography variant="h4">Place an order</Typography>
-              <OrderForm />
-            </Grid>
+              <Typography variant="h4">Specials</Typography>
+              <Typography variant="h6">
+                K40 for all deliveries in Lusaka
+              </Typography>
+            </Paper>
+          </Grid> */}
+
+          <Grid
+            item
+            xs={12}
+            md={8}
+            sx={{
+              alignItems: "center",
+              justifyContent: "center",
+              alignContent: "center",
+              display: "flex",
+              padding: "20px",
+              flexDirection: "column",
+              width: "100%",
+              flex: 1,
+            }}
+          >
+            <Typography variant="h4">Place an order</Typography>
+            <OrderForm />
           </Grid>
-        </Container>
-      </Box>
+        </Grid>
+      </Layout>
     </>
   );
 };
