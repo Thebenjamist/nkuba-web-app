eec73b654cf7fe8645125e83a2edbab56b21b3a9
Benjamin Ho <benho061995@gmail.com>
2022-09-14 13:56:34 +0200

    Created initial theme

--- src/theme.js
+++ src/theme.js
@@ -1,31 +1,81 @@
 import { createTheme } from "@mui/material/styles";
 import { red } from "@mui/material/colors";
 
+const colors = {
+  snow: "#FFFBFF",
+  eerie_black: "#131615",
+  cyber_yellow: "#FCD332",
+  light_slate_grey: "#7C90A0",
+  electric_blue: "#8AF3FF",
+};
+
 // Create a theme instance.
 const theme = createTheme({
+  components: {
+    // Name of the component
+    MuiStepLabel: {
+      styleOverrides: {
+        // Name of the slot
+        root: {
+          // Some CSS
+          fontSize: "10rem",
+          color: colors.snow,
+          "& $active": {
+            color: colors.electric_blue,
+          },
+        },
+        labelContainer: {
+          color: colors.light_slate_grey,
+        },
+      },
+    },
+    MuiInputLabel: {
+      styleOverrides: {
+        root: {
+          color: colors.light_slate_grey,
+        },
+      },
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          color: colors.eerie_black,
+          textTransform: "lowercase",
+          textTransform: "capitalize",
+        },
+      },
+    },
+    MuiIconButton: {
+      styleOverrides: {
+        root: {
+          color: colors.light_slate_grey,
+        },
+      },
+    },
+  },
   palette: {
     primary: {
-      main: "#ffd73f",
-      contrastText: "#006A79",
+      main: colors.cyber_yellow,
+      contrastText: colors.eerie_black,
     },
     secondary: {
-      main: "#006A79",
+      main: colors.snow,
     },
     third: {
-      main: "#A8CD00",
+      main: colors.eerie_black,
     },
     accents: {
-      light: "#00E0FF",
-      dark: "#FC8C79",
+      light: colors.light_slate_grey,
+      dark: colors.electric_blue,
     },
     background: {
-      default: "#ffd73f",
-      paper: "#F2FEDC",
+      default: colors.snow,
+      paper: colors.snow,
     },
     text: {
-      primary: "#006A79",
-      secondary: "#A8CD00",
-      alt: "#00E0FF",
+      primary: colors.eerie_black,
+      secondary: colors.light_slate_grey,
+      alt: colors.light_slate_grey,
     },
   },
   typography: {
