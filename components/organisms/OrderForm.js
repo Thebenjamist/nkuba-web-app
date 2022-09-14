08e48ade09905039564cd7b96212a14c36dcee72
Benjamin Ho <benho061995@gmail.com>
2022-09-14 13:56:22 +0200

    Created parent order form

--- components/organisms/OrderForm.js
+++ components/organisms/OrderForm.js
@@ -1,51 +1,85 @@
-import { Paper, Grid, TextField, Button, Typography } from "@mui/material";
+import * as React from "react";
+import { Paper, Grid, TextField, Typography, Container } from "@mui/material";
+
+import PrimaryButton from "../atoms/PrimaryButton";
+import StepperMain from "../molecules/Stepper";
+import PickupForm from "../molecules/Forms/PickupForm";
+
+import dynamic from "next/dynamic";
+import RecipientForm from "../molecules/Forms/RecipientForm";
+import WhenForm from "../molecules/Forms/WhenForm";
+import OrderSummary from "../molecules/OrderSummary";
 
 const OrderForm = () => {
+  const steps = ["Pickup", "When", "Drop-off"];
+
+  const [activeStep, setActiveStep] = React.useState(0);
+  const [orderForm, setOrderForm] = React.useState();
+
+  const nextStep = () => {
+    const newActiveStep = isLastStep() ? activeStep : activeStep + 1;
+    setActiveStep(newActiveStep);
+  };
+
+  const previousStep = () => {
+    const newActiveStep = activeStep - 1;
+    setActiveStep(newActiveStep);
+  };
+
+  const totalSteps = () => steps.length;
+
+  const isLastStep = () => {
+    return activeStep === totalSteps();
+  };
+
   return (
-    <Paper sx={{ padding: "16px", marginTop: "8px" }}>
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <TextField placeholder="From" fullWidth />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField placeholder="To" fullWidth />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField placeholder="Date" fullWidth />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField placeholder="Time" fullWidth />
-        </Grid>
-        <Grid
-          item
-          xs={12}
-          sx={{
-            alignItems: "center",
-            justifyContent: "center",
-            display: "flex",
-          }}
-        >
-          <Button
-            sx={{
-              backgroundColor: "secondary.main",
-              minWidth: "140px",
-              textTransform: "none",
-              padding: "12px",
-              "&:hover": {
-                backgroundColor: "third.main",
-              },
-            }}
-          >
-            <Typography
-              sx={{
-                fontWeight: 700,
-                color: "background.paper",
-              }}
-            >
-              Submit
-            </Typography>
-          </Button>
-        </Grid>
+    <Paper
+      sx={{
+        padding: "16px",
+        marginTop: "8px",
+        backgroundColor: "secondary.light",
+        width: "100%",
+        height: "100%",
+      }}
+    >
+      <StepperMain activeStep={activeStep} steps={steps} />
+      <Grid container sx={{ width: "100%" }}>
+        {activeStep === 0 && (
+          <PickupForm
+            step={activeStep}
+            setActiveStep={setActiveStep}
+            orderForm={orderForm}
+            setOrderForm={setOrderForm}
+          />
+        )}
+        {console.log("Order: ", orderForm)}
+
+        {activeStep === 1 && (
+          <WhenForm
+            step={activeStep}
+            setActiveStep={setActiveStep}
+            orderForm={orderForm}
+            setOrderForm={setOrderForm}
+          />
+        )}
+
+        {activeStep === 2 && (
+          <RecipientForm
+            step={activeStep}
+            setActiveStep={setActiveStep}
+            orderForm={orderForm}
+            setOrderForm={setOrderForm}
+          />
+        )}
+
+        {activeStep === 3 && (
+          <OrderSummary
+            step={activeStep}
+            setActiveStep={setActiveStep}
+            orderForm={orderForm}
+            setOrderForm={setOrderForm}
+          />
+        )}
       </Grid>
     </Paper>
   );
