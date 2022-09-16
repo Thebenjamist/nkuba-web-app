1f471fb0d32ab0891f4069671702008a6eaf3438
Benjamin Ho <benho061995@gmail.com>
2022-09-16 02:07:13 +0200

    Removed commented code

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,12 @@ import * as React from "react";
 import { Grid, Paper, Typography } from "@mui/material";
 import OrderForm from "../components/organisms/OrderForm";
 import Layout from "../components/organisms/Layout";
-
 const Home = () => {
   return (
     <>
       <Layout>
         <Grid
           container
-          // style={{ height: "100%" }}
-          // direction={{ xs: "column-reverse", md: "row" }}
           sx={{
             height: "100%",
             display: "flex",
@@ -20,35 +17,6 @@ const Home = () => {
             width: "100%",
           }}
         >
-          {/* <Grid
-            item
-            xs={12}
-            md={6}
-            sx={{
-              alignItems: "center",
-              justifyContent: "center",
-              alignContent: "center",
-              display: "flex",
-              padding: "16px",
-            }}
-          >
-            <Paper
-              sx={{
-                height: 300,
-                maxWidth: 300,
-                padding: "20px",
-                margin: 0,
-                textAlign: "center",
-                backgroundColor: "secondary.main",
-              }}
-            >
-              <Typography variant="h4">Specials</Typography>
-              <Typography variant="h6">
-                K40 for all deliveries in Lusaka
-              </Typography>
-            </Paper>
-          </Grid> */}
-
           <Grid
             item
             xs={12}
