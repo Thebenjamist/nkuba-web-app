1e1ac21a37ded59fec5821d79c0c10f7b5bfa0e3
Benjamin Ho <benho061995@gmail.com>
2022-09-16 02:13:43 +0200

    Update orders.js

--- services/api/orders.js
+++ services/api/orders.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const base_url = "http://localhost:3000";
+const base_url = process.env.NEXT_PUBLIC_API_URL;
 
 export const createOrder = async ({ data }) => {
   const res = await axios
