2842866a83b07b3ac42c2e46e7ebdd3271e4820a
Benjamin Ho <benho061995@gmail.com>
2022-09-21 18:10:07 +0200

    Created dropdown menu on mobile devices

--- components/organisms/AppBarTop.js
+++ components/organisms/AppBarTop.js
@@ -5,11 +5,14 @@ import {
   Toolbar,
   Typography,
   Button,
-  IconButton,
   Grid,
-  Hidden,
+  Menu,
+  MenuItem,
+  IconButton,
 } from "@mui/material";
 import { useRouter } from "next/router";
+
+import { AccountCircle, Menu as MenuIcon } from "@mui/icons-material";
 
 import AppBarButton from "../atoms/AppBarButton";
 import { BoltOutlined } from "@mui/icons-material";
@@ -17,6 +20,20 @@ import { Router } from "next/router";
 import NkubaLogo from "../../src/logos/1";
 const AppBarTop = () => {
   const router = useRouter();
+
+  const handleChange = (event) => {
+    setAuth(event.target.checked);
+  };
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <AppBar
       position="relative"
@@ -49,12 +66,12 @@ const AppBarTop = () => {
         <Grid
           container
           sx={{
-            display: "flex",
+            display: { xs: "none", sm: "flex" },
             justifyContent: "center",
           }}
         >
           {router.route !== "/" && (
-            <Grid item xs={12}>
+            <Grid item xs={4}>
               <AppBarButton navigate="/">Order</AppBarButton>
             </Grid>
           )}
@@ -66,21 +83,81 @@ const AppBarTop = () => {
           )} */}
 
           {router.route !== "/track" && (
-            <Grid item xs={12}>
+            <Grid item xs={4}>
               <AppBarButton navigate="track">Track</AppBarButton>
             </Grid>
           )}
 
-          {/* {router.route !== "/register" && (
-            <Grid item xs={3}>
+          {router.route !== "/login" && (
+            <Grid item xs={4}>
+              <AppBarButton navigate="login">Login</AppBarButton>
+            </Grid>
+          )}
+
+          {router.route !== "/register" && (
+            <Grid item xs={4}>
               <AppBarButton navigate="register">Register</AppBarButton>
             </Grid>
           )}
-          {router.route !== "/login" && (
-            <Grid item xs={3}>
-              <AppBarButton navigate="login">Login</AppBarButton>
-            </Grid>
-          )} */}
+        </Grid>
+
+        <Grid
+          container
+          sx={{
+            display: { xs: "flex", sm: "none" },
+            justifyContent: "flex-end",
+          }}
+        >
+          <Grid item xs={4}>
+            <IconButton
+              size="large"
+              onClick={handleMenu}
+              sx={{
+                height: "100%",
+                color: "text.primary",
+              }}
+            >
+              <MenuIcon />
+            </IconButton>
+
+            <Menu
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              {router.route !== "/" && (
+                <MenuItem onClick={() => router.push("/")}>
+                  <Typography sx={{ fontWeight: 600 }}>Order</Typography>
+                </MenuItem>
+              )}
+              {router.route !== "/track" && (
+                <MenuItem onClick={() => router.push("/track")}>
+                  <Typography sx={{ fontWeight: 600 }}>Track</Typography>
+                </MenuItem>
+              )}
+
+              {router.route !== "/login" && (
+                <MenuItem onClick={() => router.push("/login")}>
+                  <Typography sx={{ fontWeight: 600 }}>Login</Typography>
+                </MenuItem>
+              )}
+
+              {router.route !== "/register" && (
+                <MenuItem onClick={() => router.push("/register")}>
+                  <Typography sx={{ fontWeight: 600 }}>Register</Typography>
+                </MenuItem>
+              )}
+            </Menu>
+          </Grid>
         </Grid>
       </Box>
     </AppBar>
