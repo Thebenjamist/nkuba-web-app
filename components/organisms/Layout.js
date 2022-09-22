db5c8c190bc962fc5f9e93f0f26d5ce328c4a761
Benjamin Ho <benho061995@gmail.com>
2022-09-23 00:08:24 +0200

    Added user state to Layout

--- components/organisms/Layout.js
+++ components/organisms/Layout.js
@@ -2,19 +2,28 @@ import Head from "next/head";
 import AppBar from "./AppBarTop";
 import ContainerMain from "../atoms/ContainerMain";
 import Body from "../atoms/Body";
+import { UserContext } from "../../services/contexts/userContext";
+import React from "react";
+import Loading from "./Loading";
 
-const Layout = ({ children }) => (
-  <>
-    <Head>
-      <title>Nkuba Logistics</title>
-      <link rel="shortcut icon" href="images/icons/bolt.png" />
-    </Head>
+const Layout = ({ children }) => {
+  const { session } = React.useContext(UserContext);
 
-    <Body>
-      <AppBar />
-      <ContainerMain>{children}</ContainerMain>
-    </Body>
-  </>
-);
+  return (
+    <>
+      <Head>
+        <title>Nkuba Logistics</title>
+        <link rel="shortcut icon" href="images/icons/bolt.png" />
+      </Head>
+
+      <Body>
+        <AppBar />
+        <ContainerMain>
+          {session === "loading" ? <Loading /> : children}
+        </ContainerMain>
+      </Body>
+    </>
+  );
+};
 
 export default Layout;
